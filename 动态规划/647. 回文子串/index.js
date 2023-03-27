d47475ddf93ecf1ba7641d6df4d7c3f6c8fc869d
xmweijh <xmweijh@foxmail.com>
2023-03-27 09:54:11 +0800

    动态规划：647

--- 动态规划/647. 回文子串/index.js
+++ 动态规划/647. 回文子串/index.js
@@ -4,21 +4,20 @@
  */
 function countSubstrings(s) {
     let count = 0;
-    for (let i = 0; i < s.length; i++) {
-        // 以s[i]为中心，查找奇数长度的回文子串
-        count += countPalindromicSubstrings(s, i, i);
-        // 以s[i]和s[i+1]为中心，查找偶数长度的回文子串
-        count += countPalindromicSubstrings(s, i, i + 1);
-    }
-    return count;
-}
+    const len = s.length;
+    let dp = new Array(len).fill().map(() => new Array(len).fill(false))
 
-function countPalindromicSubstrings(s, left, right) {
-    let count = 0;
-    while (left >= 0 && right < s.length && s[left] === s[right]) {
-        count++;
-        left--;
-        right++;
+    for(let i = len-1; i >= 0; i--) {
+        for(let j = i; j< len; j++) {
+            if(s[i] === s[j]) {
+                if(i === j) dp[i][j] = true;
+                else if(i === j-1) dp[i][j] = true;
+                else {
+                    dp[i][j] = dp[i+1][j-1]
+                }
+                if(dp[i][j] === true) count++;
+            }
+        }
     }
     return count;
 }
