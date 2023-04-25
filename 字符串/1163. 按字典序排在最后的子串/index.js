204faf3df862dd57abbb315c559fc97d8a39e190
xmweijh <xmweijh@foxmail.com>
2023-04-25 23:09:35 +0800

    双指针：1163

---
+++ 字符串/1163. 按字典序排在最后的子串/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var lastSubstring = function (s) {
+    const n = s.length;
+    let i = 0;
+    for (let j = 1, k = 0; j + k < n; ) {
+        if (s[i + k] === s[j + k]) {
+            ++k;
+        } else if (s[i + k] < s[j + k]) {
+            i += k + 1;
+            k = 0;
+            if (i >= j) {
+                j = i + 1;
+            }
+        } else {
+            j += k + 1;
+            k = 0;
+        }
+    }
+    return s.slice(i);
+};
+
+let s = "cacacb"
+console.log(lastSubstring(s))
